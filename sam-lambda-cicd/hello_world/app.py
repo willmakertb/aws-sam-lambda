cbbbfe37d9db38ecaf00ae73151b4993e0e5e652
Willmaker <willmakertb@gmail.com>
2024-12-04 20:58:28 -0600

    add event to  function lambda

--- sam-lambda-cicd/hello_world/app.py
+++ sam-lambda-cicd/hello_world/app.py
@@ -37,6 +37,8 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello W",
+            "event": event,
+            "date": "2024-12-04",
             # "location": ip.text.replace("\n", "")
         }),
     }
